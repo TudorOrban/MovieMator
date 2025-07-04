831a7be08e10692eea08f05b65c29b70ee4726c8
TudorOrban <tudororban2@gmail.com>
2025-07-04 10:00:04 +0300

    fix(infrastructure): Adjust deploy code x3

--- backend/scripts/stop_application.sh
+++ backend/scripts/stop_application.sh
@@ -2,6 +2,9 @@
 # This script stops the currently running Docker container for the backend application.
 # It's called by CodeDeploy in the BeforeInstall hook.
 
+echo "Installing jq for JSON parsing..."
+sudo yum install -y jq
+
 echo "Stopping existing Docker container (if any)..."
 
 CONTAINER_ID=$(docker ps -aq --filter "name=moviemator-spring-boot-app")
